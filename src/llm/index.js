17d7e591a95eb3aed59d7c383a180503760127ed
Eric Mesquita <ericmesqui2468@gmail.com>
2024-07-29 01:37:29 -0300

    Update env.example and llm change the variable name for Perspective API key and moved recommendation and classification modules to llm module

--- src/llm/index.js
+++ src/llm/index.js
@@ -1,7 +1,8 @@
-const axios = require("axios")
 const Groq = require("groq-sdk")
+const fs = require("fs")
+const path = require("path")
 
-module.exports = async function llmRequest(prompt, toxicComment) {
+async function llmRequest(prompt, toxicComment) {
     const groq = new Groq({
         apiKey: process.env.GROQ_API_KEY
     })
@@ -24,6 +25,48 @@ module.exports = async function llmRequest(prompt, toxicComment) {
         return llmResponse
     } catch (error) {
         console.error("Error fetching groq response:", error)
+        throw error // Rethrow the error or handle it as needed
+    }
+}
+
+function readPromptClassification() {
+    const filePath = path.join(__dirname, "prompt-classification.json")
+    const fileContent = fs.readFileSync(filePath, "utf-8")
+    return JSON.parse(fileContent)
+}
+
+function readPromptRecommendation() {
+    const filePath = path.join(__dirname, "prompt-recommendation.json")
+    const fileContent = fs.readFileSync(filePath, "utf-8")
+    return JSON.parse(fileContent)
+}
+
+module.exports = async function getCommentClassification(toxicComment) {
+    try {
+        // Classification prompt is taken from the llm/prompt-classification.json file
+        const classificationPrompt = readPromptClassification()
+        const classification = llmRequest(
+            classificationPrompt.prompt,
+            toxicComment
+        )
+        return classification
+    } catch (error) {
+        console.error("Error fetching classification:", error)
+        throw error // Rethrow the error or handle it as needed
+    }
+}
+
+module.exports = async function getFriendlyComment(toxicComment) {
+    try {
+        // Fetch a friendly comment from the Groq API
+        const recommendationPrompt = readPromptRecommendation()
+        const friendlyComment = llmRequest(
+            recommendationPrompt.prompt,
+            toxicComment
+        )
+        return friendlyComment
+    } catch (error) {
+        console.error("Error fetching friendly comment:", error)
         throw error // Rethrow the error or handle it as needed
     }
 }
--- src/monitoring/index.js
+++ src/monitoring/index.js
@@ -1,16 +1,18 @@
 const Probot = require("probot")
 const detectToxicity = require("../detection/index")
-const getFriendlyComment = require("../recommendation/index")
-const getCommentClassification = require("../classification/index")
+const getFriendlyComment = require("../llm/index")
+const getCommentClassification = require("../llm/index")
+const llmResquest = require("../llm/index")
 const reactToUserComment = require("../reaction/index")
 const { client } = require("../mongo/connection")
+const axios = require("axios")
 
 module.exports = async function monitorComments(context) {
     try {
         // Connect to the MongoDB database
-        await client.connect()
-        const db = client.db("PeaceMaker")
-        const collection = db.collection("comments")
+        // await client.connect()
+        // const db = client.db("PeaceMaker")
+        // const collection = db.collection("comments")
 
         // Get the comment body from the context payload
         const commentBody = context.payload.comment.body
@@ -36,20 +38,22 @@ module.exports = async function monitorComments(context) {
 
             await reactToUserComment(context, "confused")
             console.log("Toxic comment saved to database")
-            await collection.insertOne({
-                comment_id: context.payload.comment.id,
-                id_user: context.payload.comment.user.id,
-                id_repo: context.payload.repository.id,
-                user_login: context.payload.comment.user.login,
-                repo_full_name: context.payload.repository.full_name,
-                created_at: context.payload.comment.created_at,
-                comment: commentBody,
-                classification: classification,
-                toxicityScore,
-                friendlyComment: friendlyComment,
-                solved: false,
-                solution: null // Fixed, ignored or disputed
-            })
+            // Requisiton to save the comment using the API
+
+            // await collection.insertOne({
+            //     comment_id: context.payload.comment.id,
+            //     id_user: context.payload.comment.user.id,
+            //     id_repo: context.payload.repository.id,
+            //     user_login: context.payload.comment.user.login,
+            //     repo_full_name: context.payload.repository.full_name,
+            //     created_at: context.payload.comment.created_at,
+            //     comment: commentBody,
+            //     classification: classification,
+            //     toxicityScore,
+            //     friendlyComment: friendlyComment,
+            //     solved: false,
+            //     solution: null // Fixed, ignored or disputed
+            // })
         }
     } catch (error) {
         console.error("Error processing comment:", error)
