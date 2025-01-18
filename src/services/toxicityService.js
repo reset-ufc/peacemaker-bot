540ebea7141696a6b035b22a123e508628083222
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-18 00:19:56 -0300

    lint and formated

--- src/services/toxicityService.js
+++ src/services/toxicityService.js
@@ -1,28 +1,28 @@
-const { google } = require("googleapis");
-const { DISCOVERY_URL, PERSPECTIVE_API_KEY } = require("../config/constants");
+const { google } = require('googleapis');
+const { DISCOVERY_URL, PERSPECTIVE_API_KEY } = require('../config/constants');
 
 async function detectToxicity(comment) {
-    try {
-        const client = await google.discoverAPI(DISCOVERY_URL);
-        const analyzeRequest = {
-            comment: { text: comment },
-            requestedAttributes: { TOXICITY: {} }
-        };
+  try {
+    const client = await google.discoverAPI(DISCOVERY_URL);
+    const analyzeRequest = {
+      comment: { text: comment },
+      requestedAttributes: { TOXICITY: {} },
+    };
 
-        const response = await client.comments.analyze({
-            key: PERSPECTIVE_API_KEY,
-            resource: analyzeRequest
-        });
+    const response = await client.comments.analyze({
+      key: PERSPECTIVE_API_KEY,
+      resource: analyzeRequest,
+    });
 
-        if (!response.data?.attributeScores?.TOXICITY) {
-            throw new Error("Unable to get toxicity score");
-        }
-
-        return response.data;
-    } catch (error) {
-        console.error("Toxicity detection error:", error);
-        throw error;
+    if (!response.data?.attributeScores?.TOXICITY) {
+      throw new Error('Unable to get toxicity score');
     }
+
+    return response.data;
+  } catch (error) {
+    console.error('Toxicity detection error:', error);
+    throw error;
+  }
 }
 
 module.exports = { detectToxicity };
