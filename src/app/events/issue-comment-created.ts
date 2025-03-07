305497bd1b313e1a5192680d1401f96cb6e0de4a
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-07 12:33:37 -0300

    adiciona análise de toxicidade e classificação de comentários no evento de criação de comentários

--- src/app/events/issue-comment-created.ts
+++ src/app/events/issue-comment-created.ts
@@ -1,4 +1,6 @@
+import { Comment, CommentType } from '../../models/comment';
 import { mock_analyzeToxicity as analyzeToxicity } from '../../services/google-perspective';
+import { generateClassification } from '../../services/groq';
 
 export const handleIssueComment = async (context: any) => {
   const TOXICITY_THRESHOLD = 0.6;
@@ -11,70 +13,78 @@ export const handleIssueComment = async (context: any) => {
     return;
   }
 
-  const pespertiveResponse = await analyzeToxicity(comment.body);
-
+  // Verifica a toxicidade do comentário
+  const pespertiveResponse = await analyzeToxicity(comment.body.trim());
   console.log(
     'pespertiveResponse',
-    JSON.stringify({ pespertiveResponse }, null, 2),
+    JSON.stringify(pespertiveResponse, null, 2),
   );
 
-  // // Dados do comentário
-  // const commentData = {
-  //   gh_comment_id: comment.id,
-  //   content: comment.body || '',
-  //   comment_created_at: new Date(comment.created_at),
-  //   author_id: author.id.toString(),
+  // classifica o comentario como uma das categorias descritas
+  // 'bitter_frustration', 'mocking', 'irony', 'insulting', 'vulgarity', 'identity_attack', 'entitlement', 'impatience', 'threat', 'neutral',
+  const classification = await generateClassification(
+    comment.body.trim(),
+    pespertiveResponse.language,
+  );
+  console.log('classification', JSON.stringify(classification, null, 2));
 
-  //   // Contexto do objeto pai (Issue ou PR)
-  //   parent: {
-  //     gh_parent_id: issue.number,
-  //     type: issue.pull_request ? CommentType.PULL_REQUEST : CommentType.ISSUE,
-  //     title: issue.title,
-  //     url: issue.html_url,
-  //   },
+  // Dados do comentário
+  const commentData = {
+    gh_comment_id: comment.id,
+    content: comment.body.trim(),
+    comment_created_at: new Date(comment.created_at),
+    author_id: author.id.toString(),
 
-  //   // Dados do repositório
-  //   repository_fullname: repository.full_name,
-  //   is_repository_private: repository.private,
-  //   repository_owner: repository.owner.login,
+    // Contexto do objeto pai (Issue ou PR)
+    parent: {
+      gh_parent_id: issue.number,
+      type: issue.pull_request ? CommentType.PULL_REQUEST : CommentType.ISSUE,
+      title: issue.title,
+      url: issue.html_url,
+    },
 
-  //   // Análise de toxicidade
-  //   toxicity_score: pespertiveResponse.toxicityScore,
-  //   toxicity_analyzed_at: new Date(),
-  //   flagged: pespertiveResponse.toxicityScore >= TOXICITY_THRESHOLD,
-  //   classification: 'neutral',
+    // Dados do repositório
+    repository_fullname: repository.full_name,
+    is_repository_private: repository.private,
+    repository_owner: repository.owner.login,
 
-  //   solutioned: false,
-  //   solution: '',
-  //   solution_analyzed_at: null,
+    // Análise de toxicidade
+    toxicity_score: pespertiveResponse.toxicityScore,
+    toxicity_analyzed_at: new Date(),
+    flagged: pespertiveResponse.toxicityScore >= TOXICITY_THRESHOLD,
+    classification: classification.trim(),
 
-  //   // Metadados do evento
-  //   event_type: context.name, // 'issue_comment.created'
-  //   installation_id: installation.id,
-  //   moderated: false, // Inicialmente não moderado
-  //   moderation_action: null,
-  // };
+    // solução do comentário
+    solutioned: false,
 
-  // console.log('comment', JSON.stringify({ comment: commentData }, null, 2));
+    // Metadados do evento
+    event_type: context.name, // 'issue_comment.created'
+    installation_id: installation.id,
+    moderated: false, // Inicialmente não moderado
+    moderation_action: null,
+  };
 
-  // if (pespertiveResponse.toxicityScore >= TOXICITY_THRESHOLD) {
-  //   // Enviar notificação de moderação
-  //   const { data: notification } =
-  //     await context.octokit.reactions.createForIssueComment({
-  //       owner: repository.owner.login,
-  //       repo: repository.name,
-  //       comment_id: comment.id,
-  //       content: 'eyes',
-  //     });
+  const commentCreated = await Comment.create(commentData);
+  console.log('comment created', JSON.stringify(commentCreated, null, 2));
 
-  //   console.log('notification', JSON.stringify({ notification }, null, 2));
+  if (pespertiveResponse.toxicityScore >= TOXICITY_THRESHOLD) {
+    // Enviar notificação de moderação
+    const { data: notification } =
+      await context.octokit.reactions.createForIssueComment({
+        owner: repository.owner.login,
+        repo: repository.name,
+        comment_id: comment.id,
+        content: 'eyes',
+      });
+    console.log('notification', JSON.stringify(notification, null, 2));
 
-  //   // Adiciona um comentário para notificar que a moderação foi feita
-  //   await context.octokit.issues.createComment({
-  //     owner: repository.owner.login,
-  //     repo: repository.name,
-  //     issue_number: issue.number,
-  //     body: `@${context.payload.sender.login} Hi there! We noticed some potentially concerning language in your recent comment. Would you mind reviewing our guidelines at https://github.com/apps/thepeacemakerbot? Let's work together to maintain a positive atmosphere.`,
-  //   });
-  // }
+    // Adiciona um comentário para notificar que a moderação foi feita
+    const botComment = await context.octokit.issues.createComment({
+      owner: repository.owner.login,
+      repo: repository.name,
+      issue_number: issue.number,
+      body: `@${context.payload.sender.login} Hi there! We noticed some potentially concerning language in your recent comment. Would you mind reviewing our guidelines at https://github.com/apps/thepeacemakerbot? Let's work together to maintain a positive atmosphere.`,
+    });
+    console.log('botComment', JSON.stringify(botComment, null, 2));
+  }
 };
