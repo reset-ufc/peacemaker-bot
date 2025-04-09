1db627224a3f31ad71cbf8b102003016f69f6b74
Antonio880 <antoniocruzgomes880@gmail.com>
2025-04-09 09:04:24 -0300

    add is_rejected field to suggestions and comments models; update handleComment function to include it

--- src/models/suggestions.ts
+++ src/models/suggestions.ts
@@ -11,6 +11,7 @@ const SuggestionsSchema = new Schema<SuggestionsDocument>({
   gh_comment_id: { type: String, required: true },
   content: { type: String, required: true },
   is_edited: { type: Boolean, required: true },
+  is_rejected: { type: Boolean, default: false },
   created_at: { type: Date, default: Date.now },
 });
 
